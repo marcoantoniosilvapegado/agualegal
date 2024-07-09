314659a9ab7ef460e4efd9a759ec88688bd8385a
marcoantoniosilvapegado <marcoantoniosilvapegado@gmail.com>
2024-07-09 17:28:21 -0300

    Fase inicial

---
+++ src/main/java/br/gov/go/sefaz/agualegal/AgualegalApplication.java
@@ -0,0 +1,13 @@
+package br.gov.go.sefaz.agualegal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgualegalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgualegalApplication.class, args);
+	}
+
+}
